82e83a5f41faec7e668d031ac121d20e14aeedf7
Michael DeMarco <michaelfromyeg@gmail.com>
2024-01-06 19:31:50 -0800

    feat: more yearS

--- client/src/components/Form.tsx
+++ client/src/components/Form.tsx
@@ -45,12 +45,24 @@ const MODES: Options<Option> = [
 
 const YEARS: Options<Option> = [
   {
+    value: "2024",
+    label: "2024",
+  },
+  {
     value: "2023",
     label: "2023",
   },
   {
     value: "2022",
     label: "2022",
+  },
+  {
+    value: "2021",
+    label: "2021",
+  },
+  {
+    value: "2020",
+    label: "2020",
   },
 ];
 
